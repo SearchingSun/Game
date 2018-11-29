49531167c34dcae9179d63d60f20f712230081e5
SearchingSun <andres556@yandex.ru>
2018-11-30 02:33:06 +0300

    omg

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 function startGame () {
+    application ();
+    back ()
+}
+
+function application () {
     var app = new PIXI.Application({
-        width: 800,         // default: 800
+        width: 900,         // default: 800
         height: 600,        // default: 600
         antialias: true,    // default: false
         transparent: false, // default: false
         resolution: 1       // default: 1
     });
-    app.renderer.backgroundColor = 0x061639;
+
     var body = document.getElementById("body");
-    document.getElementById("button").style.display = "none"
+    document.getElementById("button").style.display = "none";
     body.appendChild(app.view);
-};
+}
+
+function back (){
+    var dungeonTexture = TextureCache["../images/background.png"];
+    dungeon = new Sprite(dungeonTexture);
+    app.stage.addChild(dungeon);
+}
